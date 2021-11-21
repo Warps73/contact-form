9815840cf7cf86a4c65b2f7cec5d1af41e783b3a
Timothy <timothy.prefol@gmail.com>
2021-11-21 12:15:12 +0100

    Merge migrations

--- migrations/Version20211120130730.php
+++ migrations/Version20211120130730.php
@@ -20,9 +20,20 @@ final class Version20211120130730 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE contact_message (id INT AUTO_INCREMENT NOT NULL, contact_user_id INT NOT NULL, message LONGTEXT NOT NULL, name VARCHAR(255) NOT NULL, INDEX IDX_2C9211FE3D41F214 (contact_user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE contact_user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_A56C54B6E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE contact_message ADD CONSTRAINT FK_2C9211FE3D41F214 FOREIGN KEY (contact_user_id) REFERENCES contact_user (id)');
+        $this->addSql(
+            'CREATE TABLE contact_message (id INT AUTO_INCREMENT NOT NULL, contact_user_id INT NOT NULL, message LONGTEXT NOT NULL, name VARCHAR(255) NOT NULL, INDEX IDX_2C9211FE3D41F214 (contact_user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB'
+        );
+        $this->addSql(
+            'CREATE TABLE contact_user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_A56C54B6E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB'
+        );
+        $this->addSql(
+            'ALTER TABLE contact_message ADD CONSTRAINT FK_2C9211FE3D41F214 FOREIGN KEY (contact_user_id) REFERENCES contact_user (id)'
+        );
+        $this->addSql('ALTER TABLE contact_message ADD processed TINYINT(1) DEFAULT \'0\' NOT NULL');
+        $this->addSql(
+            'CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB'
+        );
+
     }
 
     public function down(Schema $schema): void
@@ -31,5 +42,7 @@ final class Version20211120130730 extends AbstractMigration
         $this->addSql('ALTER TABLE contact_message DROP FOREIGN KEY FK_2C9211FE3D41F214');
         $this->addSql('DROP TABLE contact_message');
         $this->addSql('DROP TABLE contact_user');
+        $this->addSql('DROP TABLE user');
+
     }
 }
